cbfdf78cdd926a3bdadc02935ea1e8a4a253f2e2
kdileep1994 <k.dileep1994@gmail.com>
2014-10-09 18:29:06 +0530

    modified:   fn_main.m question2

--- fn_main.m
+++ fn_main.m
@@ -6,19 +6,33 @@ global I_ext a b gamma
 a = 0.5;
 b = 0.1;
 gamma = 0.1;
-time_interval = [0 200];
+time_interval = [0 500];
 inital_conditions = [0 0];
-[T, solution] = ode45(@fn_ode, time_interval, inital_conditions);
-figure(1)
-plot(T, solution(:, 1), 'b')
-hold on
-plot(T, solution(:, 2), 'r')
-figure(2)
-plot(solution(:,2), solution(:,1))
-v = -0.4:0.01:1.5;
-w_v = v.*(a-v).*(v-1) + I_ext;
-w_omega = b/gamma * v;
-figure(3)
-plot(v, w_v, 'r')
-hold on
-plot(v, w_omega, 'b')
+I1 = 0;
+I2 = 0;
+for I_ext=1:0.01:5
+    [T, solution] = ode45(@fn_ode, time_interval, inital_conditions);
+    [peak, location] = findpeaks(solution(:, 1));
+    if length(peak) > 1 && I1 == 0
+        I1 = I_ext;
+    end
+    if length(peak) <= 1 && I1 ~= 0
+        I2 = I_ext;
+        break;
+    end
+end
+% figure(1)
+% plot(T, solution(:, 1), 'b')
+% hold on
+% plot(T, solution(:, 2), 'r')
+% figure(2)
+% plot(solution(:,2), solution(:,1))
+% v = -0.4:0.01:1.5;
+% w_v = v.*(a-v).*(v-1) + I_ext;
+% w_omega = b/gamma * v;
+% figure(3)
+% plot(v, w_v, 'r')
+% hold on
+% plot(v, w_omega, 'b')
+I1
+I2
