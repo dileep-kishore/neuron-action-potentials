3fb60ea290baa05d12acb4e4082748bb3b33ba03
kdileep1994 <k.dileep1994@gmail.com>
2014-10-10 02:26:21 +0530

    I1 working I2 not

--- Q2/fn_main2.m
+++ Q2/fn_main2.m
@@ -13,20 +13,19 @@ I2 = 0;
 thres = 0.5;
 for I_ext=0:0.01:5
     [T, solution] = ode45(@fn_ode, time_interval, inital_conditions);
-    if max(solution(:, 1)) < thres
+    if max(solution(:, 1)) < 0.3
         pks = [];
     else
         [pks, l] = findpeaks(solution(:, 1), 'MINPEAKHEIGHT', 0.3);
-        pks = sort(pks, 2, 'descend')
+        pks = sort(pks, 2, 'descend');
     end
-    if length(pks) > 1 && I1 == 0 && pks(1) > thres
+    if length(pks) > 1 && I1 == 0 && pks(2) > thres
+        pks
         I1 = I_ext;
     end
-    if length(pks) >= 2
-        if pks(2) < thres && I1 ~= 0
-            I2 = I_ext;
-            break;
-        end
+    if length(pks) > 1 && pks(2) < thres && I1 ~= 0
+        I2 = I_ext;
+        break;
     end
 end
 I1
