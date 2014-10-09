f7d4f0bbfce2aab052083b59bfb84a34840bf581
kdileep1994 <k.dileep1994@gmail.com>
2014-10-09 18:30:03 +0530

    modified:   fn_ode.m question2

--- fn_ode.m
+++ fn_ode.m
@@ -4,12 +4,12 @@ function dy = fn_ode(t, y)
     dy = zeros(2,1);
 	a = 0.5;
 	b = 0.1;
-    % if t > 100 && t < 100
-    %     I_ext = 0.5;
-    % else
-    %     I_ext = 0;
-    % end
+    if t > 100 && t < 400
+        I = I_ext;
+    else
+        I = 0;
+    end
 	gamma = 0.1;
-	dy(1) = y(1) * (a - y(1)) * (y(1) - 1) - y(2) + I_ext;
+	dy(1) = y(1) * (a - y(1)) * (y(1) - 1) - y(2) + I;
 	dy(2) = b * y(1) - gamma * y(2);
 end
